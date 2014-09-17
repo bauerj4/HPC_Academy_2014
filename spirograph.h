e43028b50928c07c51143442dd59e8be5a3372e6
V2870 (Conference ID) <v2870@pcmathsg14.maths.pwf.cam.ac.uk>
2014-09-17 15:30:25 +0100

    Spirograph file initial push.

---
+++ spirograph.h
@@ -0,0 +1,28 @@
+
+/*
+  This file shall contain the mathematics for the spirograph and it shall
+  execute print statments from drawingIO to create the postscript.  
+
+*/
+
+#include "drawingIO.h"
+
+double static X(double t); // x parametrization
+double static Y(double t); // y parametrization
+
+
+/*
+  Computes a new stepsize based on the current values of the state variables.
+  This is necessary because of the order of magnitude varying length scales
+  in fractal geometries, and with the spirograph in particular.
+*/
+
+double static calculateTimeStep(double x, dounble y, double t, double *timeStep);
+
+/*
+  Performs calculation of spirogaph at a timestep.  Calls timestep recalculation
+  and updates variables passed by reference.  Postscript lines are printed
+  as well.
+*/
+
+void advanceStep(double *x, double *y, double *t);
